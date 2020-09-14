964f0ffefe24ac82ac56cf7534805a92e42b9ad0
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-09-14 08:14:52 +0200

    Started working on a payment component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./components/payment/Payment";
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -38,6 +39,10 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Payment />
           </Route>
           <Route path="/">
             <Header />
