3adbc6b6fc5a8e1cf5b419344c3732ce9108624d
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-09-17 16:36:35 +0200

    Fixed app name and favicon

--- amazon-clone/src/axios.js
+++ amazon-clone/src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/amznclonesiksnis/us-central1/api", // The API (cloud function) URL
+  baseURL: "https://us-central1-amznclonesiksnis.cloudfunctions.net/api",
+  // http://localhost:5001/amznclonesiksnis/us-central1/api
 });
 
 export default instance;
+
+// http://localhost:5001/amznclonesiksnis/us-central1/api
